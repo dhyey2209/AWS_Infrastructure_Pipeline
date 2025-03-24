06c4cfaa904f914bf54b70590e9bd4c02c90b3db
dhyey2209 <dhyey2209@gmail.com>
2025-03-24 09:27:27 -0400

    changed bucket name

--- aws_devops/aws_devops_stack.py
+++ aws_devops/aws_devops_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    RemovalPolicy
 )
 from constructs import Construct
 
@@ -10,10 +10,12 @@ class AwsDevopsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsDevopsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an S3 bucket
+        bucket = s3.Bucket(
+            self,
+            "cdk-code-catalyst",
+            bucket_name="testing",  # Replace with your desired bucket name
+            versioned=True,  # Enable versioning
+            encryption=s3.BucketEncryption.S3_MANAGED,  # Enable encryption
+            removal_policy=RemovalPolicy.DESTROY  # Optional: This will allow CDK to delete the bucket when you destroy the stack
+        )
